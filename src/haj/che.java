c37e8b12a69378a95760a5a398ea7109cad6a31d
Lenovo <Lenovo@LAPTOP-V2EAIF8Q>
2023-09-30 20:37:14 +0900

    2023-9-30

---
+++ src/haj/che.java
@@ -0,0 +1,41 @@
+package haj;
+
+public class che {
+	private double jiage;
+	private String pinpai;
+	private String yanse;
+
+	public che() {
+
+	}
+
+	public che(int jiage, String pinpai, String yanse) {
+		this.jiage = jiage;
+		this.pinpai = pinpai;
+		this.jiage = jiage;
+	}
+
+	public double getJiage() {
+		return jiage;
+	}
+
+	public void setJiage(double jiage) {
+		this.jiage = jiage;
+	}
+
+	public String getPinpai() {
+		return pinpai;
+	}
+
+	public void setPinpai(String pinpai) {
+		this.pinpai = pinpai;
+	}
+
+	public String getYanse() {
+		return yanse;
+	}
+
+	public void setYanse(String yanse) {
+		this.yanse = yanse;
+	}
+}
---
+++ src/haj/cheTest.java
@@ -0,0 +1,32 @@
+package haj;
+
+import java.util.Scanner;
+
+public class cheTest {
+    public static void main(String[] args) {
+        //��������
+        che [] arr = new che[3];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            che c = new che();//ע��������������һ��Ҫ����Forѭ����,�������в���ȷ
+            System.out.println("������۸�");
+            double qian = sc.nextDouble();
+            c.setJiage(qian);
+
+            System.out.println("Ʒ��");
+            String pinpai = sc.next();
+            c.setPinpai(pinpai);
+
+            System.out.println("��ɫ");
+            String yanse = sc.next();
+            c.setYanse(yanse);//ע����������������ֵ��Ҫ��Ӧ��Ӧ���������
+            arr[i] = c;//����ǽ������������ֵ������ϵ
+        }
+
+        for(int i = 0; i < arr.length ; i++){
+            che car=arr[i];
+            System.out.println(car.getJiage()+ "   "+ car.getPinpai()
+                    +"  " + car.getYanse());
+        }
+    }
+}
