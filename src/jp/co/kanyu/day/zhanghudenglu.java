c2e494872fa727f07bc8972517107527bd997029
Lenovo <Lenovo@LAPTOP-V2EAIF8Q>
2023-10-01 20:50:19 +0900

    账号登录

--- src/jp/co/kanyu/day/zhanghudenglu.java
+++ src/jp/co/kanyu/day/zhanghudenglu.java
@@ -8,19 +8,19 @@ public class zhanghudenglu {
         String userpassword = "123456";
 
         Scanner sr = new Scanner(System.in);
-        System.out.println("�������˺ź�����");
+        System.out.println(请输入账号和密码");
         for(int i = 0; i < 3; i++){
 
             String torokuid = sr.next();
             String torokupassword = sr.next();
            if(torokuid .equals(rightuserID) && torokupassword .equals(userpassword)){
-            System.out.println("��¼�ɹ�");
+            System.out.println("登录成功");
             break;
         }else{
             if (i == 2 ) {
-                System.out.println("�����˻�������");
+                System.out.println("您的账户被锁定");
                 }else{
-                System.out.println("��¼ʧ��,����������" + "��ʣ" + (2 - i) + "�λ���");
+                System.out.println("登录失败,请重新输入" + "还剩" + (2 - i) + "次机会");
              }
             }
         }
