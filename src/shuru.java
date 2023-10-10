fc6fd6f9525f84cd1df8bb49622854c7d0367b19
Lenovo <Lenovo@LAPTOP-V2EAIF8Q>
2023-10-10 22:18:10 +0900

    20231010

---
+++ src/shuru.java
@@ -0,0 +1,31 @@
+
+
+public class shuru {
+    public static void main(String[] args) {
+        int[][] arr = {{1,1,1}, {1, 1, 2}, {1,1 ,3}, {1, 1, 4}};
+        int temp = 0;
+        int sun =0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                temp = arr[i][j] + temp;
+            }
+        }
+        System.out.println(temp);
+
+
+          int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int [] arrnum = arr[i];
+           num =  getsum(arrnum);
+           System.out.println(num);
+        }
+
+    }
+    public static int getsum(int [] arr){
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+}
